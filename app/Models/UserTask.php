9298e7e3e5437cdd6348a8687d691aeade8900e1
ReinzeeMathewChua <chuarc.962.stud@cdd.edu.ph>
2024-07-31 10:33:50 +0800

    Validation

---
+++ app/Models/UserTask.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class UserTask extends Model
+{
+    use HasFactory;
+
+    protected $table = 'user_task';
+    protected $guarded =[];
+    
+
+    // protected $fillable = [
+    //     'user_id',
+    //     'tas_id',
+    //     'status_id',
+    // ]
+
+}
