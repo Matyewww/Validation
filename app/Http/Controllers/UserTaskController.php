d4a47e9cf75f648d916adeee225869ccd90772f3
Chua Reinzee <chuarc.962.stud@cdd.edu.ph>
2024-08-01 10:42:01 +0800

    Create display blade for index

--- app/Http/Controllers/UserTaskController.php
+++ app/Http/Controllers/UserTaskController.php
@@ -11,7 +11,9 @@ class UserTaskController extends Controller
      */
     public function index()
     {
-        return UserTask::all();
+        $data['user_tasks'] = UserTask::all();
+        return view('index', $data);
+        //return UserTask::all();
     }
 
     /**
