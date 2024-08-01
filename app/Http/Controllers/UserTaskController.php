3ad2c626186c73cc028201b44bcd76e8f4ed09a7
Chua Reinzee <chuarc.962.stud@cdd.edu.ph>
2024-08-01 10:15:50 +0800

    display all data

--- app/Http/Controllers/UserTaskController.php
+++ app/Http/Controllers/UserTaskController.php
@@ -11,7 +11,7 @@ class UserTaskController extends Controller
      */
     public function index()
     {
-        //
+        return UserTask::all();
     }
 
     /**
@@ -49,7 +49,7 @@ class UserTaskController extends Controller
      */
     public function show(string $id)
     {
-        //
+        
     }
 
     /**
